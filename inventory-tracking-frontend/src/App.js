c5b12e6da69d91c3dc1b5dd1470b63984eef326a
dandlp95 <del17011@byui.edu>
2022-05-17 15:35:42 -0600

    App Routes

--- inventory-tracking-frontend/src/App.js
+++ inventory-tracking-frontend/src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import OptionsBar from "./components/OptionsBar";
+import ProductsLayout from "./components/ProductsLayout";
+import WarehousesLayout from "./components/WarehousesLayout";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <OptionsBar />
+      <Routes>
+        <Route path="/products" element={<ProductsLayout/>}/>
+        <Route path="/warehouses" element={<WarehousesLayout/>}/>
+      </Routes>
+    </Router>
   );
 }
 
