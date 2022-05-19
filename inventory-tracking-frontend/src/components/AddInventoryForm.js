af3978b338c61bc02fbe7cab19751f8033559667
dandlp95 <del17011@byui.edu>
2022-05-18 19:23:33 -0600

    almost

--- inventory-tracking-frontend/src/components/AddInventoryForm.js
+++ inventory-tracking-frontend/src/components/AddInventoryForm.js
@@ -1,10 +1,43 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useParams, Link, Route, Routes } from "react-router-dom";
 import axios from "axios";
 
 function AddInventoryForm() {
   const urlAllWarehouses = "http://localhost:8080/warehouses";
   const [warehouses, setWarehouses] = useState(null);
+
+  const { id } = useParams(); // This is id of the product
+
+  const quantityInputRef = useRef();
+  const warehouseInputRef = useRef();
+
+  function addInventoryHandler(qty, url){
+      const options = {
+          method: "PUT",
+          headers: {
+              "Content-Type": "application/json"
+          },
+          body: JSON.stringify(qty)
+      };
+      fetch(url, options)
+
+  }
+
+  function submissionHandler(e) {
+    e.preventDefault();
+
+    const enteredQuantity = quantityInputRef.current.value;
+    const enteredWarehouse = warehouseInputRef.current.value;
+    const productId = id;
+
+    const quantity = {
+      quantity: enteredQuantity,
+    };
+
+    const urlReq = `http://localhost:8080/warehouses/${enteredWarehouse}/${productId}`;
+
+    addInventoryHandler(quantity, urlReq);
+  }
 
   useEffect(() => {
     axios.get(urlAllWarehouses).then((response) => {
@@ -13,11 +46,21 @@ function AddInventoryForm() {
   }, [urlAllWarehouses]);
 
   if (warehouses) {
-      return(
-          <form>
-              <label ></label>
-          </form>
-      )
+    return (
+      <form onSubmit={submissionHandler}>
+        <label htmlFor="quantity">Quantity: </label>
+        <input type="number" id="quantity" required ref={quantityInputRef} />
+
+        <label htmlFor="warehouse">Select the Warehouse: </label>
+        <select name="warehouse" id="warehouse" ref={warehouseInputRef}>
+          {warehouses.map((warehouse) => {
+            return <option value={warehouse._id}>{warehouse.name}</option>;
+          })}
+        </select>
+
+        <input type="submit" value="Submit" />
+      </form>
+    );
   }
 }
 
